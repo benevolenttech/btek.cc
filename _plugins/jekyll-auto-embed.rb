c592267fc47912df8be9427e42ca3e9801c23b9c
BrianDombrowski <bdombro@gmail.com>
2017-05-15 01:02:18 -0400

    Added auto-embed plugin

---
+++ _plugins/jekyll-auto-embed.rb
@@ -0,0 +1,37 @@
+# Will auto-embed youtube and vimeo links in <p></p>s
+# Inspired by jekyll-mentions
+
+require "jekyll"
+
+module Jekyll
+  class Embeds
+
+    InvalidJekyllEmbedConfig = Class.new(Jekyll::Errors::FatalException)
+
+    class << self
+      def embedify(doc)
+        return unless doc.output.include?("https://vimeo.com/") | doc.output.include?("https://youtube.com/")
+        # Vimeo
+        doc.output = doc.output.gsub(/<p>(https:\/\/)(vimeo.com)(\/.*)<\/p>/, "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='\\1player.\\2\/video\\3' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div><br>")
+        doc.output = doc.output.gsub(/<p>(https:\/\/)(youtube.com)(\/watch\?v\=)(.*)<\/p>/, "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='\\1www.\\2\/embed\/\\4' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div><br>")
+      end
+
+      # Public: Defines the conditions for a document to be embedable.
+      #
+      # doc - the Jekyll::Document or Jekyll::Page
+      #
+      # Returns true if the doc is written & is HTML.
+      def embedable?(doc)
+        (doc.is_a?(Jekyll::Page) || doc.write?) &&
+          doc.output_ext == ".html" || (doc.permalink && doc.permalink.end_with?("/"))
+      end
+
+      private
+
+    end
+  end
+end
+
+Jekyll::Hooks.register %i[pages documents], :post_render do |doc|
+  Jekyll::Embeds.embedify(doc) if Jekyll::Embeds.embedable?(doc)
+end
