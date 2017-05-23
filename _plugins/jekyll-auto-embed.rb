5e60b50a321a1df99f201728190ed643da849123
BrianDombrowski <bdombro@gmail.com>
2017-05-23 16:34:16 -0400

    Assuming youtube links have www in them now

--- _plugins/jekyll-auto-embed.rb
+++ _plugins/jekyll-auto-embed.rb
@@ -10,11 +10,11 @@ module Jekyll
 
     class << self
       def embedify(doc)
-        return unless doc.output.include?("https://vimeo.com/") | doc.output.include?("https://youtube.com/")
+        return unless doc.output.include?("https://vimeo.com/") | doc.output.include?("https://www.youtube.com/")
         # Vimeo
         doc.output = doc.output.gsub(/<p>(https:\/\/)(vimeo.com)(\/.*)<\/p>/, "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='\\1player.\\2\/video\\3' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div><br>")
         # Youtube
-        doc.output = doc.output.gsub(/<p>(https:\/\/)(youtube.com)(\/watch\?v\=)(.*)<\/p>/, "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='\\1www.\\2\/embed\/\\4' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div><br>")
+        doc.output = doc.output.gsub(/<p>(https:\/\/www\.)(youtube.com)(\/watch\?v\=)(.*)<\/p>/, "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='\\1\\2\/embed\/\\4' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div><br>")
       end
 
       # Public: Defines the conditions for a document to be embedable.
