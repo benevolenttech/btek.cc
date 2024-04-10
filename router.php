0839d2fce092793b0c17d2e6c841be8c581f6e93
Brian Dombrowski <brian@bzz.fm>
2024-04-10 17:48:32 -0500

    bugfix

--- router.php
+++ router.php
@@ -8,6 +8,11 @@
 
 // php -t public -S localhost:8000 router.php 
 
+$pathname = explode('?', $_SERVER['REQUEST_URI'])[0];
+if ($pathname === '/') {
+  $pathname = '/index';
+}
+
 // if (preg_match(
 //   '/\.(?:3gp|apk|avi|bmp|css|csv|doc|docx|flac|gif|gz|gzip|htm|html|ics|jpe|jpeg|jpg|js|json|kml|kmz|m4a|mov|mp3|mp4|mpeg|mpg|odp|ods|odt|oga|ogg|ogv|pdf|pdf|png|pps|pptx|qt|svg|swf|tar|text|tif|txt|wav|webm|wmv|xls|xlsx|xml|xsl|xsd|zip)$/',
 //   $_SERVER["REQUEST_URI"]
@@ -16,8 +21,7 @@
 // }
 
 // if file exists at public/$_SERVER["REQUEST_URI"], return false
-error_log('req:' . $_SERVER["REQUEST_URI"]);
-if (file_exists('public' . $_SERVER["REQUEST_URI"])) {
+if (file_exists('public' . $pathname)) {
   return false;
 }
 
@@ -30,10 +34,6 @@ include_once 'app/util/nav.php';
 /**
  * Look for a route file that matches the request URI and render it
  */
-$pathname = explode('?', $_SERVER['REQUEST_URI'])[0];
-if ($pathname === '/') {
-  $pathname = '/index';
-}
 
 $route = null;
 $posArgs = [];
